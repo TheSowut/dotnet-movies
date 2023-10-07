f929197f86406915cdb447b3c772820e7f93bbbd
TheSowut <sowutmail@gmail.com>
2023-10-07 16:07:50 +0300

    chore: add request & resposne types

--- Movies.Contracts/Responses/MovieResponse.cs
+++ Movies.Contracts/Responses/MovieResponse.cs
@@ -1,7 +1,9 @@
-namespace Movies.Contracts.Requests;
+namespace Movies.Contracts.Responses;
 
-public class UpdateMovieRequest
+public class MovieResponse
 {
+    public required Guid Id { get; init; } 
+    
     public required string Title { get; init; }
     
     public required int YearOfRelease { get; init; }
